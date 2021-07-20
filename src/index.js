261058b35ae3e43eaa0e65fdcd0bff6595797c9c
DmitriEG1991 <78304061+DmitriEG1991@users.noreply.github.com>
2021-07-20 20:04:57 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -37,8 +37,49 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(elem) {
+    
+    let mass = [];
+    let res = "";
+    let str2 = "";
+    let el = elem.split("");
+    let result = "";
+
+    function first(expr){
+        for(let i=9; i>-1; i-=2){
+            let str = "";
+            str += expr[i-1];
+            str += expr[i];
+            if(str == "10"){
+                mass.unshift(".");
+            }
+            if(str == "11"){
+                mass.unshift("-");
+            }
+        }
+        str2 = mass.join("");
+        mass = [];
+        return str2;
+    }
+    function second(b){
+        for(let key in MORSE_TABLE){
+            if(key == b){
+               res = MORSE_TABLE[key];
+            }
+        }
+        return res; 
+    } 
+
+    for(let i=0; i<el.length; i+=10){
+        let mass2 = el.slice(i, i+10);
+        let stroka = mass2.join("");
+        if(stroka == "**********"){
+            result += " ";
+        }else{
+            result += second(first(stroka));
+        }
+    }
+    return result;
 }
 
 module.exports = {
